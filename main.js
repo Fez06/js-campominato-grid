5b831dd2938cf750ed89840244e4e7f7f1e33a38
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-02 16:02:02 +0100

    si non sto iniziando perche' voglio fissarmi idee

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-'use script';
+'use strict';
+
+
+// Funzioni //
+
+function creaAppendi(htmlElement, containerElement) {
+    const casella = document.createElement(htmlElement);
+    containerElement.append(htmlElement);
+}
+
+function assegnaClasse(classe, target) {
+    target.classList.add(classe);
+}
+
+function facTotum (htmlElement, htmlClass) {
+    const boardCell = document.createElement(htmlElement);
+    boardCell.classList.add(htmlClass);
+    containerBoard.append(boardCell);
+}
+//questi erano tentativi falliti
+
+
+// Main  //
+const containerBoard = document.querySelector('.board');
+
+const casella = document.createElement('div');
+casella.classList.add('cell');
+containerBoard.append(casella);
+
+facTotum('p', 'cell');
+creaAppendi('p', containerBoard);
+assegnaClasse('')
