27149a0e28608e81875b016bdc65692cf4bb7bc0
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-02 17:23:20 +0100

    ecco

--- main.js
+++ main.js
@@ -1,35 +1,56 @@
 'use strict';
 
 
+
+
+// function creaAppendi(htmlElement, containerElement) {
+//     const casella = document.createElement(htmlElement);
+//     containerElement.append(htmlElement);
+//     return casella;
+// }
+
+
+// function assegnaClasse(classe, target) {
+//     target.classList.add(classe);
+// }
+
+// function facTotum (htmlElement, htmlClass) {
+//     const boardCell = document.createElement(htmlElement);
+//     boardCell.classList.add(htmlClass);
+//     containerBoard.append(boardCell);
+// }
+// //questi erano tentativi falliti
+
+
+// // Main  //
+// const containerBoard = document.querySelector('.board');
+
+// const casella = document.createElement('div');
+// casella.classList.add('cell');
+// containerBoard.append(casella);
+
+// facTotum('p', 'cell');
+// const x = creaAppendi('p', containerBoard);
+// assegnaClasse('.cell', x);
+
+
+
 // Funzioni //
 
-function creaAppendi(htmlElement, containerElement) {
-    const casella = document.createElement(htmlElement);
-    containerElement.append(htmlElement);
-    return casella;
-}
-
-
-function assegnaClasse(classe, target) {
-    target.classList.add(classe);
-}
-
-function facTotum (htmlElement, htmlClass) {
+function facTotum (htmlElement, cssClass, htmlContainer) {
     const boardCell = document.createElement(htmlElement);
-    boardCell.classList.add(htmlClass);
-    containerBoard.append(boardCell);
+    boardCell.classList.add(cssClass);
+    htmlContainer.append(boardCell);
 }
-//questi erano tentativi falliti
 
 
-// Main  //
+
+
+// // Main  //
+
 const containerBoard = document.querySelector('.board');
 
-const casella = document.createElement('div');
-casella.classList.add('cell');
-containerBoard.append(casella);
-
-facTotum('p', 'cell');
-const x = creaAppendi('p', containerBoard);
-assegnaClasse('.cell', x);
-
+for (let i = 1; i <= 100; i++) {
+    facTotum('div','cell', containerBoard );
+    document.querySelector('cell').innerText = i;
+}
